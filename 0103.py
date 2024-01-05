749e028b93503bfbe8b8442d0b8511150beb4f38
paramir <yhntgb444@gmail.com>
2024-01-05 12:43:55 +0900

    added comment

--- 0103.py
+++ 0103.py
@@ -14,6 +14,7 @@ def LCS(a: str, b:str):
     # find LCS
     for i in range(1,h):
         for j in range(1,w):
+            #a,b는 index 0부터 해야해서 -1
             if a[j-1] == b[i-1]:
                 dp[i][j] = dp[i-1][j-1] + 1
             else:
